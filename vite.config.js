8afd8d9ec8e1e110b85d9e823a43dfc705f1c4df
TarasMysiuraTutor <tarasmysiuratutor@gmail.com>
2026-01-02 19:58:24 +0200

    correct

--- vite.config.js
+++ vite.config.js
@@ -3,7 +3,7 @@ import react from "@vitejs/plugin-react";
 
 export default defineConfig({
   plugins: [react()],
-  base: "/EngSim/",
+  base: process.env.NODE_ENV === 'production' ? '/EngSim/' : '/',
   build: {
     sourcemap: false,
     minify: "esbuild",
